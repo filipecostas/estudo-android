9006c8720388011a6d28f8f249170d81cf098a41
Filipe Costa Inmetro <flp.costas@gmail.com>
2013-12-27 12:10:02 -0200

    adicionando menu para dashboard

--- EstudoAndroid01/gen/br/gov/inmetro/estudoandroid01/R.java
+++ EstudoAndroid01/gen/br/gov/inmetro/estudoandroid01/R.java
@@ -19,24 +19,30 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int main_horizontal_margin=0x7f040002;
+        public static final int main_vertical_margin=0x7f040003;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int btnSalvar=0x7f080008;
+        public static final int action_settings=0x7f08000d;
+        public static final int btnAeroGear=0x7f080002;
+        public static final int btnDroidPersistence=0x7f080004;
+        public static final int btnJpaAndroid=0x7f080003;
+        public static final int btnSalvar=0x7f08000c;
         public static final int btnSqlite=0x7f080000;
-        public static final int cadastrar=0x7f08000b;
-        public static final int editAno=0x7f080007;
-        public static final int editMarca=0x7f080005;
-        public static final int editNome=0x7f080003;
-        public static final int listar=0x7f08000c;
-        public static final int listview=0x7f080001;
-        public static final int principal=0x7f08000a;
-        public static final int textAno=0x7f080006;
-        public static final int textMarca=0x7f080004;
-        public static final int txtNome=0x7f080002;
+        public static final int btnStorage=0x7f080001;
+        public static final int cadastrar=0x7f08000f;
+        public static final int editAno=0x7f08000b;
+        public static final int editMarca=0x7f080009;
+        public static final int editNome=0x7f080007;
+        public static final int listar=0x7f080010;
+        public static final int listview=0x7f080005;
+        public static final int principal=0x7f08000e;
+        public static final int textAno=0x7f08000a;
+        public static final int textMarca=0x7f080008;
+        public static final int txtNome=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -49,14 +55,23 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        /**  aerogear 
+         */
+        public static final int aerogear=0x7f050017;
         public static final int app_name=0x7f050001;
         public static final int botaoSalvar=0x7f050011;
         public static final int cadastrar=0x7f050009;
         public static final int campoAno=0x7f050010;
         public static final int campoMarca=0x7f05000f;
         public static final int campoNome=0x7f05000e;
+        /**  droid persistence 
+         */
+        public static final int droidPersistence=0x7f050018;
         public static final int editar=0x7f05000a;
         public static final int excluir=0x7f05000b;
+        /**  jpa 
+         */
+        public static final int jpaAndroid=0x7f050016;
         public static final int listar=0x7f05000d;
         public static final int msgAtencao=0x7f050006;
         /**  msgs 
@@ -72,6 +87,9 @@ public final class R {
         /**  sqlite 
          */
         public static final int sqlite=0x7f050008;
+        /**  storage 
+         */
+        public static final int storage=0x7f050015;
         public static final int verDetalhes=0x7f05000c;
     }
     public static final class style {
